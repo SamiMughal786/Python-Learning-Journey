194adc280837f8fadd63fa13e4a5475aaabc1c18
Sami mughal <129625097+SamiMughal786@users.noreply.github.com>
2025-06-28 03:00:42 +0500

    Add files via upload

---
+++ 1stDay.py
@@ -0,0 +1,51 @@
+#Day 1 of Learnig Python
+ 
+#Welcome to my Python Programming Journey
+name="Sami ullah"
+age=22
+weight=78
+height=5.10
+cgpa=3.19
+university="Gujrat"
+city="Sambrial"
+is_student=False
+
+print("Name:", name)
+print("Age:", age)
+print("Weight:", weight)
+print("Height:", height)
+print("CGPA:", cgpa)
+print("University:", university)
+print("City:", city)
+print("Student:", is_student)
+
+#check the data types of these Variables
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+#check my Age
+
+year=2025
+year_i_born=2003
+x=year-year_i_born
+print("My Age is:",x)
+
+#Simple Bio Card Generator
+
+name = "Sami ullah"
+father_name = "Shafiue"
+age = 22
+blood_group = "A+"
+print("===== BIO CARD =====")
+print("Name:", name)
+print("Father's Name:", father_name)
+print("Age:", age)
+print("Blood Group:", blood_group)
+
+#CGPA to Percentage Calculator
+
+cgpa=3.19
+percentage=cgpa*25
+print("your percentage is:",percentage,"%")
