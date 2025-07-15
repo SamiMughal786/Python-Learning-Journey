852ade3f0f154e82dca9c3d32928f0fe2db141c6
Sami mughal <129625097+SamiMughal786@users.noreply.github.com>
2025-07-16 02:03:34 +0500

    Add files via upload

---
+++ While loops.py
@@ -0,0 +1,27 @@
+#while loops 
+z=5
+while(z > 0):
+    print(z)
+    z= z - 1
+
+#Example
+i=int(input("Enter your Number: "))
+while(i<=35):
+    i=int(input("Enter your Number: "))
+    print(i)
+
+print("Done with the loop")
+
+# else statement in while loop
+x=-7
+while(x > 0):
+    print(x)
+    x=x + 1
+else:
+    print("This is else statement")
+
+# do-while loop not use in python but u know the question 
+# Emulate do while loops in python 
+# do{
+#     #loop body:
+# }while(condition):
